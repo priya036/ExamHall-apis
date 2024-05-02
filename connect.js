ae641fb557096d8f0a0dee4d1a2a391b1f7d3d8b
priya036 <priyadharshinis.21aid@kongu.edu>
2024-05-02 22:13:30 +0530

    modified connect

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
 module.exports.connectdb = ()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/Hall")
+    return mongoose.connect("mongodb+srv://priyadharshini:5799@cluster0.x8tsbwj.mongodb.net/ExamHall-Organizer")
 }
